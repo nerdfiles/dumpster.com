bbca9456fe449c0e26b42f927d2f9dd7d6f7c556
Aaron Alexander <nerdfiles@gmail.com>
2011-06-09 13:52:09 -0500

    cleanup

--- _assets/js/ui-dev.js
+++ _assets/js/ui-dev.js
@@ -1,10 +1,14 @@
 // plugins
 
-$script('_assets/js-lib/jquery.tablesorter.min.js', 'jquery-tablesorter-min', function() {
-    $script('_assets/js-lib/jquery.tablesorter.pager.js', 'jquery-tablesorter-pager', function() {
-        $(function(e) {
-            $('.mod_itemTable').tablesorter();
+try {
+
+    $script('_assets/js-lib/jquery.tablesorter.min.js', 'jquery-tablesorter-min', function() {
+        $script('_assets/js-lib/jquery.tablesorter.pager.js', 'jquery-tablesorter-pager', function() {
+            $(function(e) {
+                $('.mod_itemTable').tablesorter();
+            });
         });
     });
-});
+
+} catch(e) { }
 
