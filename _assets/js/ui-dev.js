12ae800dca5203fb3c382bf2c5e058e3f35397be
Aaron Alexander <nerdfiles@gmail.com>
2011-06-09 14:54:17 -0500

    cleanup

--- _assets/js/ui-dev.js
+++ _assets/js/ui-dev.js
@@ -1,18 +1,14 @@
 // plugins
 
-try {
+var origin = window.location.origin;
+var pathname = (origin.indexOf('localhost') != -1) ? '/dumpster.com/' : '/ui/prototypes/dumpster.com/';
+var projectPath = origin + pathname;
 
-    var origin = window.location.origin;
-    var pathname = (origin.indexOf('localhost') != -1) ? '/dumpster.com/' : '/ui/prototypes/dumpster.com/';
-    var projectPath = origin + pathname;
-
-    $script(projectPath + '_assets/js-lib/jquery.tablesorter.min.js', 'jquery-tablesorter-min', function() {
-        $script(projectPath + '_assets/js-lib/jquery.tablesorter.pager.js', 'jquery-tablesorter-pager', function() {
-            $(function(e) {
-                $('.mod_itemTable').tablesorter();
-            });
+$script(projectPath + '_assets/js-lib/jquery.tablesorter.min.js', 'jquery-tablesorter-min', function() {
+    $script(projectPath + '_assets/js-lib/jquery.tablesorter.pager.js', 'jquery-tablesorter-pager', function() {
+        $(function(e) {
+            $('.mod_itemTable').tablesorter();
         });
     });
-
-} catch(e) { }
+});
 
