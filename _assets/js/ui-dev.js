854b310b6a1228946fe0f991f6a123f3c07bbc1b
Aaron Alexander <nerdfiles@gmail.com>
2011-06-09 15:34:21 -0500

    cleanup

--- _assets/js/ui-dev.js
+++ _assets/js/ui-dev.js
@@ -1,8 +1,8 @@
 // plugins
 
-var origin = window.location.hostname;
-var pathname = (origin.indexOf('localhost') != -1) ? '/dumpster.com/' : '/ui/prototypes/dumpster.com/';
-var projectPath = origin + pathname;
+var host = window.location.host;
+var pathname = (host.indexOf('localhost') != -1) ? '/dumpster.com/' : '/ui/prototypes/dumpster.com/';
+var projectPath = 'http://' + host + pathname;
 
 $script(projectPath + '_assets/js-lib/jquery.tablesorter.min.js', 'jquery-tablesorter-min', function() {
     $script(projectPath + '_assets/js-lib/jquery.tablesorter.pager.js', 'jquery-tablesorter-pager', function() {
